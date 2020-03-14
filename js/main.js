9ca37586de1949ce056886d262bbf99d22712d5f
Maia <lucas-mfp@hotmail.com>
2020-03-14 00:41:49 -0300

    Definindo estado do gameScreen

---
+++ js/main.js
@@ -0,0 +1,17 @@
+function main(){
+
+  var gameScreen = document.getElementById("gameScreen");
+  setStateGameScreen(gameScreen)
+
+}
+
+function setStateGameScreen(gameScreen){
+
+  gameScreen.style.height = "600px"
+  gameScreen.style.width = "600px"
+  gameScreen.style.backgroundColor = "#000"
+
+
+}
+
+main();
